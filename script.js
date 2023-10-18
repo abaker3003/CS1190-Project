a93ba036cadb42cca65160549fc8a88aef9279de
deltafliers <deltafliers@gmail.com>
2023-10-18 21:08:40 +0000

    updated formatting

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
 /**********************************************************/
-/* Hide/Show Drop Menu Landing Page */
-function openNav() {
-  document.getElementById("Main_Menu").style.width = "250px";
-}
 
-
-function closeNav() {
-  document.getElementById("Main_Menu").style.width="0";
-}
 
 /**********************************************************/
-/* Loans Page - Info */
+
+
 var acc = document.getElementsByClassName("accordian");
 var i;
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
+    var icon = this.querySelector("i");
+
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      icon.classList.remove("fa-minus");
+      icon.classList.add("fa-plus");
     } else {
       panel.style.display = "block";
+      icon.classList.remove("fa-plus");
+      icon.classList.add("fa-minus");
     }
   });
 }
 
+
+/* Loans Calculator */
+function Calculate() {
+  // Extracting value in the amount section in the variable
+  const amount = parseFloat(document.querySelector("#amount").value);
+
+  // Extracting value in the interest rate section in the variable
+  const rate = parseFloat(document.querySelector("#rate").value);
+
+  const rt = rate / 100.0;
+
+  // Extracting value in the months section in the variable
+  const months = parseInt(document.querySelector("#months").value);
+
+  // Calculating interest
+  const interest = amount * rt * months;
+
+  // Calculating monthly payment
+  const payment = (amount + interest) / months;
+
+
+  document.querySelector("#interest").innerHTML = "Total Interest: $" + interest.toFixed(2);
+  document.querySelector("#payment").innerHTML = "Monthly Payments: $" + payment.toFixed(2);
+}
+
+
+/* Compound Interest Calculator */
+function Compound_Interest() {
+  // Extracting value in the amount section in the variable
+  const amount = parseFloat(document.querySelector("#amount").value);
+
+  // Extracting value in the interest rate section in the variable
+  const rate = parseFloat(document.querySelector("#rate").value);
+
+  const rt = rate / 100.0;
+
+  // Extracting value in the months section in the variable
+  const months = parseInt(document.querySelector("#months").value);
+
+
+  const balance = amount * Math.pow(1 + rt, months);
+
+  document.querySelector("#balance").innerHTML = "Ending balance: $" + balance.toFixed(2);
+}
 /**********************************************************/
 
